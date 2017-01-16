001fff0cdec1387d2d1954d05fd2043a0f24c323
swb1701 <swb1701@gmail.com>
2017-01-15 21:57:38 -0500

    tested audio setup as working

--- audio.py
+++ audio.py
@@ -4,7 +4,10 @@
 
 import os
 import sys
+import time
 import secrets as s
+from subprocess import Popen, PIPE
+
 from pulsectl import Pulse
 
 def run(cmd):
@@ -13,23 +16,40 @@ def run(cmd):
 
 def btctl(cmd):
     print("Sending "+cmd+" to bluetoothctl...")
-    os.system('echo -e "'+cmd+'\nquit\n"|bluetoothctl')
+    p=Popen("/usr/bin/bluetoothctl",stdout=PIPE,stdin=PIPE,stderr=PIPE)
+    p.stdin.write(cmd+"\n")
+    p.stdin.flush()
+    time.sleep(4)
+    p.stdin.write("quit\n")
+    p.stdin.flush()
+    #btcmd="echo -e '"+cmd+"\\nquit\\n'|sed 's/-e //'|bluetoothctl"
+    #print("Executing:"+btcmd)
+    #os.system(btcmd)
 
 try:
     p=Pulse()
 except:
     print("Couldn't find pulseaudio running ... trying to fix that...")
     run("pulseaudio --start &")
+    time.sleep(5)
     try:
         p=Pulse()
-        btctl("connect "+s.ECHO_BT_ADDRESS)
     except:
         print("Sorry, couldn't get pulseaudio running")
+        print(sys.exc_info()[0])
+        sys.exit(0)
+    try:
+        btctl("connect "+s.ECHO_BT_ADDRESS)
+    except:
+        print("Sorry, was unable to connect to Echo after starting pulseaudio...")
+        print(sys.exc_info()[0])
         sys.exit(0)
 print("We've verified pulse is running")
 cards=p.card_list()
+found=False
 for card in p.card_list():
     if card.name.startswith("bluez_card"):
+        found=True
         idx=card.index
         print("Found a bluetooth audio source as card #"+str(idx))
         run("pactl set-card-profile "+str(idx)+" a2dp")
@@ -39,3 +59,5 @@ for card in p.card_list():
                 print("Found "+sink.description+" as sink #"+str(sink.index))
                 run("pactl set-default-sink "+str(sink.index))
                 print("I've set the Echo is the default sink")
+if not found:
+    print("Sorry you probably need to connect to bluetooth on the Echo")
